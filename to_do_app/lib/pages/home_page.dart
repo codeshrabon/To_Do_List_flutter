ecf7cb3632f0a45221dc95cecff4d878fb019ee2
Faysal Ahmed <19101043@uap-bd.edu>
2025-07-06 19:10:10 +0600

    adding colors to appbar to others

--- to_do_app/lib/pages/home_page.dart
+++ to_do_app/lib/pages/home_page.dart
@@ -14,12 +14,21 @@ class HomePage extends StatelessWidget {
       backgroundColor: Colors.deepPurple.shade200,
       appBar: AppBar(
         title: const Text('Simple ToDo'),
+        centerTitle: true,
         backgroundColor: Colors.deepPurple,
         foregroundColor: Colors.white,
       ),
       body: ListView.builder(
+        itemCount: toDoList.length,
         itemBuilder: (BuildContext context, index) {
           return;
+          // return Padding(
+          //   padding: const EdgeInsets.all(8.0),
+          //   child: Container(
+          //     color: Colors.deepPurple,
+          //     child: Text(toDoList[index][0]),
+          //   ),
+          // );
         },
       ),
     );
